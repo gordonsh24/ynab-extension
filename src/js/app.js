c25452b110a5e15451f40dc07c9994b8fd2abc8c
Jakub Bis <jakub.b@onthegosystems.com>
2020-05-10 15:49:29 +0800

    Setup antd

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,20 @@
 import * as R from "ramda"
 import React from "react";
 import ReactDOM from "react-dom";
+import { DatePicker } from 'antd';
+
+import "../css/app.css";
 
 const root = document.getElementById("app");
 
 const list = [1, 2, 3];
-const render = R.map(el => <p>EL: {el}</p>)
+const renderList = R.map(el => <p key={el}>EL: {el}</p>)
 
-ReactDOM.render(render(list), root);
+const app = () => (
+	<div>
+		{renderList(list)}
+		<DatePicker />
+	</div>
+);
+
+ReactDOM.render(app(), root);
